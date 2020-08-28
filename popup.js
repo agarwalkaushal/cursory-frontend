eebd002e872bb7ed06f48203344a4b6deeab213d
Kaushal Agarwal <kaushal.agarwal@oyorooms.com>
2020-08-28 15:51:36 +0530

    Modal Ui designed

--- popup.js
+++ popup.js
@@ -1,10 +1,10 @@
 let cursify = document.getElementById('cursify');
 let loader = document.getElementById('loader');
 
-loader.style.visibility = "hidden";
+loader.style.display = "none";
 cursify.onclick = function(element) {
-	cursify.style.visibility = "hidden";
-	loader.style.visibility = "visible";
+	cursify.style.display = "none";
+	loader.style.display = "block";
 	chrome.tabs.query({'active': true, 'windowId': chrome.windows.WINDOW_ID_CURRENT}, function(tabs) {
 		let url = tabs[0].url;
 		const complete_url = 'https://cursory-nlp.herokuapp.com/highlights?url='+url
